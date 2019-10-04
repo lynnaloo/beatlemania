02db116bdeb4ad30eb90f982e667bc8ff604e845
Linda Nichols <lynnaloo@gmail.com>
2019-10-04 11:16:52 -0400

    [update] Update George and Paul

--- george/index.js
+++ george/index.js
@@ -3,3 +3,7 @@ exports.sing = (request, response) => {
   console.log('All you need is love');
   response.status(200).send('All you need is love!');
 };
+
+exports.event = (event, callback) => {
+  callback();
+};
